9094f17a18f7ade8cd113caaca4f07c086c78e44
itsharshpro <harshpro2004@gmail.com>
2025-02-23 10:37:25 +0530

    implemented getUserInfo API

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -44,7 +44,7 @@ exports.registerUser = async (req, res) => {
         res.status(201).json({
             id: user._id,
             user,
-            token: generateToken(user._id),
+            poiiingtoken: generateToken(user._id),
         });
     } catch (err) {
         res.status(500).json({message: "Error registering user", error: err.message});
@@ -72,11 +72,30 @@ exports.loginUser = async (req, res) => {
                 totalPollsVotes : 0,
                 totalPollsBookmarked : 0,
             },
-            token: generateToken(user._id),
+            poiiingtoken: generateToken(user._id),
         });
     } catch (err) {
         res.status(500).json({message: "Error logging in user", error: err.message});
     }
 }
 
-exports.getUserInfo = async (req, res) => {}
+exports.getUserInfo = async (req, res) => {
+    try{
+        const user = await User.findById(req.user.id).select("-password");
+        if(!user){
+            return res.status(404).json({message: "User not found"});
+        }
+
+        const userInfo = {
+            ...user.toObject(),
+            totalPollsCreated : 0,
+            totalPollsVotes : 0,
+            totalPollsBookmarked : 0,
+        }
+
+        res.status(200).json(userInfo);
+
+    } catch(error){
+        res.status(500).json({message: "Error fetching user info", error: error.message});
+    }
+}
--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -3,13 +3,19 @@ const User = require('../models/User');
 
 exports.protect = async (req, res, next) => {
     let token = req.headers.authorization?.split(" ")[1];
+    
     if(!token) return res.status(401).json({message: "Not authorized, no token"});
 
     try{
+        console.log(token);
+        
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        console.log(decoded);
+        
         req.user = await User.findById(decoded.id).select("-password");
         next();
     } catch (err) {
+        console.log(err);
         return res.status(401).json({message: "Not authorized, token failed"});
     }
 };
